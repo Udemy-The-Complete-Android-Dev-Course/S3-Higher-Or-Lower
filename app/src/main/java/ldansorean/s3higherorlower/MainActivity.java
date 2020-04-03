f3d57b55df436dcdb906e1366e5ad38d6abd91ba
Laura Dansorean <9722740+ldansorean@users.noreply.github.com>
2020-04-03 23:37:31 +0100

    Implement basic version of game.

--- app/src/main/java/ldansorean/s3higherorlower/MainActivity.java
+++ app/src/main/java/ldansorean/s3higherorlower/MainActivity.java
@@ -3,12 +3,40 @@ package ldansorean.s3higherorlower;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int randomNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Random randomGenerator = new Random();
+        randomNumber = randomGenerator.nextInt(21);
+        Log.i("number", String.valueOf(randomNumber));
+    }
+
+    public void validateGuess(View view) {
+        EditText userGuessET = findViewById(R.id.userGuess);
+        Integer userGuess = Integer.parseInt(userGuessET.getText().toString());
+
+        String message = "";
+        if (userGuess == randomNumber) {
+            message = "Congratulations! You've got it!";
+        } else if (userGuess > randomNumber) {
+            message = "Your number is too high.";
+        } else {
+            message = "Your number is too low";
+        }
+
+        Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
     }
 }
